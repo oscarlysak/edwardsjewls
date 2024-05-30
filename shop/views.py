b98a3e0dd9b697bd2db74b79c2466d4056a637c8
Tom Shorten <tom.shorten1@gmail.com>
2024-05-30 17:47:13 +0100

    search works for syle and catagory

--- shop/views.py
+++ shop/views.py
@@ -1,23 +1,29 @@
-# views.py
+# shop/views.py
 from django.shortcuts import render
-from products.models import ProductCategory, Products, ProductImage, ProductStyle
+from products.models import ProductCategory, Products, ProductImage, ProductStyle, ProductCondition
 from django.http import JsonResponse
 from django.db.models import Q
 
 def shop(request):
+    search_query = request.GET.get('q', '')
+    
+    filters = Q()
+    if search_query:
+        filters |= Q(name__icontains=search_query) | Q(description__icontains=search_query)
+        filters |= Q(product_category__name__icontains=search_query) | Q(product_style__name__icontains=search_query)
+
+    products = Products.objects.filter(filters)
     categories = ProductCategory.objects.all()
     styles = ProductStyle.objects.all()
-    search_query = request.GET.get('q', '')
-    filters = Q(name__icontains=search_query) | Q(description__icontains=search_query) if search_query else Q()
-    products = Products.objects.filter(filters)
     images = ProductImage.objects.all()
-    return render(request, 'shop/shop.html', {'categories': categories, 'styles': styles, 'products': products, 'images': images})
+    
+    return render(request, 'shop/shop.html', {'products': products, 'categories': categories, 'styles': styles, 'images': images})
 
 def filter_products(request):
     category_id = request.GET.get('category_id')
     style_id = request.GET.get('style_id')
     search_query = request.GET.get('q', '')
-    
+
     filters = Q()
     if category_id:
         filters &= Q(product_category_id=category_id)
@@ -25,9 +31,10 @@ def filter_products(request):
         filters &= Q(product_style_id=style_id)
     if search_query:
         filters &= Q(name__icontains=search_query) | Q(description__icontains=search_query)
+        filters |= Q(product_category__name__icontains=search_query) | Q(product_style__name__icontains=search_query)
 
     products = Products.objects.filter(filters)
-    
+
     product_list = []
     for product in products:
         first_image = product.images.first()
@@ -39,5 +46,5 @@ def filter_products(request):
             'SKU': product.SKU,
             'image_url': first_image.image.url if first_image else '/static/images/placeholder.svg'
         })
-    
+
     return JsonResponse({'products': product_list})
