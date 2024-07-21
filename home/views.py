fda61965ffb65c70f2bd50a12164764ab0f070d8
Oscar Lysak <oscar.lysak@gmail.com>
2024-07-21 20:16:52 +0100

    Most pages are in

--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
 def index(request):
     return render(request, 'home/index.html')
+
+def faq(request):
+    return render(request,'home/faq.html')
+
+def contact(request):
+    return render(request, 'home/contact.html')
