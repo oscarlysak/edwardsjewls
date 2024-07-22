dfa65f2feab370c5b7fc934c235742530781ab25
Tom Shorten <tom.shorten1@gmail.com>
2024-07-22 14:48:25 +0100

    announcement bar implemented (not migrated)

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Announcement
 
-# Register your models here.
+@admin.register(Announcement)
+class AnnouncementAdmin(admin.ModelAdmin):
+    list_display = ('text',)
--- home/models.py
+++ home/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Announcement(models.Model):
+    text = models.CharField(max_length=255, verbose_name="Announcement Text")
+
+    def __str__(self):
+        return self.text
